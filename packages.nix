600e4c59ca6ca1e75a63bf743e00dbe412a9ae9d
Daniel Kempkens <daniel+git@kempkens.io>
2021-12-14 19:12:41 +0100

    Refactor packages

--- packages.nix
+++ packages.nix
@@ -1,32 +1,5 @@
 { system, pkgs }:
 
 {
-  cliclick = pkgs.stdenv.mkDerivation {
-    pname = "cliclick";
-    version = "5.0.1";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "BlueM";
-      repo = "cliclick";
-      rev = "79a49ff25f550fda5b2a1806366aa9d7facc46f3";
-      sha256 = "Wj1niLXCwBYTsXB2Qkdvs85BJD7uRqGIerCTjhS20ZQ=";
-      fetchSubmodules = false;
-    };
-
-    NIX_CFLAGS_COMPILE = "-include cliclick_Prefix.pch -I Actions -I .";
-
-    buildInputs = (with pkgs; [
-      perl
-    ]) ++ (with pkgs.darwin.apple_sdk.frameworks; [
-      Carbon
-      Cocoa
-      Foundation
-      IOKit
-    ]);
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp cliclick $out/bin
-    '';
-  };
+  cliclick = import ./packages/cliclick.nix { inherit system pkgs; };
 }
