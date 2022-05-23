1797853fa1d9da8ae0cdb6b3cc565e0a8b711623
Daniel Kempkens <daniel+git@kempkens.io>
2022-05-23 11:28:27 +0200

    Bump agilebits-op

--- packages/agilebits-op.nix
+++ packages/agilebits-op.nix
@@ -2,11 +2,11 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "agilebits-op";
-  version = "2.0.1";
+  version = "2.3.0";
 
   src = pkgs.fetchurl {
     url = "https://cache.agilebits.com/dist/1P/op2/pkg/v${version}/op_apple_universal_v${version}.pkg";
-    sha256 = "Y3hc7XNab8GjdqYQtg2HOZNpzaSc/jf3ruXd3rJSvIc=";
+    sha256 = "f4hEYklyx33FT0rskR5o7Brpk8b0XbiOPNaYssgjjjY=";
   };
 
   buildInputs = with pkgs; [ xar cpio ];
