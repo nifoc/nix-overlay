a57dac5b77dc0718f1c5a0d6e809ed5e1f3d14ac
Daniel Kempkens <daniel+git@kempkens.io>
2022-06-11 17:32:28 +0200

    Add fennel-luajit overlay

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 * [agilebits-op](https://app-updates.agilebits.com/product_history/CLI2)
 * [cliclick](https://github.com/BlueM/cliclick)
+* fennel-luajit
 * [phantomjs](https://github.com/ariya/phantomjs)
 * [proximity-sort](https://github.com/jonhoo/proximity-sort)
 * [q](https://github.com/natesales/q)
--- overlay.nix
+++ overlay.nix
@@ -8,4 +8,19 @@ let custom = super.callPackage ./packages.nix { }; in
   inherit (custom) phantomjs;
   inherit (custom) proximity-sort;
   inherit (custom) q;
+
+  fennel-luajit = super.fennel.overrideAttrs (
+    let
+      luaPackages = with super.luajitPackages; [ readline ];
+    in
+    o: rec {
+      buildInputs = [ super.luajit super.makeWrapper ] ++ luaPackages;
+
+      postInstall = o.postInstall or "" + ''
+        wrapProgram $out/bin/fennel \
+          --suffix LUA_CPATH ";" "${super.lib.concatMapStringsSep ";" super.luajitPackages.getLuaCPath luaPackages}" \
+          --suffix LUA_PATH ";" "${super.lib.concatMapStringsSep ";" super.luajitPackages.getLuaPath luaPackages}"
+      '';
+    }
+  );
 }
