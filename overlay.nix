0a2fdff685b3cd4ca32e42dd1df1c7b90b7206d4
Daniel Kempkens <daniel+git@kempkens.io>
2022-08-06 17:26:50 +0200

    Make fennel-luajit a proper package

--- overlay.nix
+++ overlay.nix
@@ -1,26 +1,13 @@
 _: super:
 
-let custom = super.callPackage ./packages.nix { }; in
-
+let
+  custom = super.callPackage ./packages.nix { };
+in
 {
   inherit (custom) agilebits-op;
   inherit (custom) cliclick;
+  inherit (custom) fennel-luajit;
   inherit (custom) phantomjs;
   inherit (custom) proximity-sort;
   inherit (custom) q;
-
-  fennel-luajit = super.fennel.overrideAttrs (
-    let
-      luaPackages = with super.luajitPackages; [ readline ];
-    in
-    o: rec {
-      buildInputs = [ super.luajit super.makeWrapper ] ++ luaPackages;
-
-      postInstall = o.postInstall or "" + ''
-        wrapProgram $out/bin/fennel \
-          --suffix LUA_CPATH ";" "${super.lib.concatMapStringsSep ";" super.luajitPackages.getLuaCPath luaPackages}" \
-          --suffix LUA_PATH ";" "${super.lib.concatMapStringsSep ";" super.luajitPackages.getLuaPath luaPackages}"
-      '';
-    }
-  );
 }
