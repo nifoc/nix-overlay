00b2645374f89918feab744d843754e05cf1cc73
Daniel Kempkens <daniel+git@kempkens.io>
2023-02-27 11:39:51 +0100

    jsregexp: Update to 0.0.6

--- packages/luarocks-jsregexp.nix
+++ packages/luarocks-jsregexp.nix
@@ -2,14 +2,14 @@
 
 pkgs.lua51Packages.buildLuarocksPackage rec {
   pname = "jsregexp";
-  version = "0.0.5";
+  version = "0.0.6";
   rockspecFilename = "./${pname}-${version}-1.rockspec";
 
   src = pkgs.fetchFromGitHub {
     owner = "kmarius";
     repo = pname;
-    rev = "3f8942325ba720b4173e6370a47d5a0529da2efb";
-    sha256 = "sha256-WlPWp3iJRZH5CfQohD81qIrE1KRbBOZWvFUU7LjEkq0=";
+    rev = "b096131504f5d2842fe225d40eb5a9eccca7db27";
+    hash = "sha256-T47r87FtrFnEw+a1CguqtPr3Wt/04M1xwLYw00K8aOQ=";
   };
 
   meta = with lib; {
