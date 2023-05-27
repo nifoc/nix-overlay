e21d2aa92a9126800f928cae101574e203154c85
Daniel Kempkens <daniel+git@kempkens.io>
2023-05-27 21:13:53 +0200

    rimgo: Update to 2023-05-26

--- packages/rimgo.nix
+++ packages/rimgo.nix
@@ -2,17 +2,17 @@
 
 pkgs.buildGoModule rec {
   pname = "rimgo";
-  version = "2023-03-14";
+  version = "2023-05-26";
 
   src = pkgs.fetchFromGitea {
     domain = "codeberg.org";
     owner = "video-prize-ranch";
     repo = pname;
-    rev = "4e065bf455bd16826a07026c30d6f160174d1dde";
-    hash = "sha256-uUabhiMdlMX06Cq3DmKL8AH9I4TwJrV7FSCKalgpkcU=";
+    rev = "f0188614b055b6f5d295c46f72ad162d0daa5ac3";
+    sha256 = "sha256-EWsx7FUkdSjZmhmJ+KZhswU94jez5dfrUaD3SyX0RI4=";
   };
 
-  vendorSha256 = "sha256-7oArN4V8gvDZJJ/1A9kVz/d/U9Nd69duH+oUSX7TgE0=";
+  vendorSha256 = "sha256-qfWYDfIdkZ1pHV97OTv0M8Qi8+2diNUuJmorQMX91AI=";
 
   meta = with lib; {
     description = "An alternative frontend for Imgur.";
