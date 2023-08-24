a3b788eeb7b400b0fa0e0c982b6f05a6e2783bf4
Daniel Kempkens <daniel+git@kempkens.io>
2023-08-24 21:21:15 +0200

    AnonymousOverflow: Update to 1.9.0

--- packages/anonymous-overflow.nix
+++ packages/anonymous-overflow.nix
@@ -2,13 +2,13 @@
 
 pkgs.buildGoModule rec {
   pname = "anonymous-overflow";
-  version = "1.8.3";
+  version = "1.9.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "httpjamesm";
     repo = "AnonymousOverflow";
     rev = "v${version}";
-    sha256 = "sha256-qPwKkxWvavCtYrALr9ZWk0MUo1GDlCR9vg0Lh/BLoAk=";
+    sha256 = "sha256-7em/F2+VSM4kE7Hd2Aw0ka2+gWSxG3VMBs5iQnYwEqw=";
   };
 
   vendorSha256 = "sha256-dIzK8r3pPmV8B2xhpSK7Vg82RnsZ2kptEYOkYPZrPik=";
