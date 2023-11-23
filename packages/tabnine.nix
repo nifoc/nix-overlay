223db86bb6237f3ce263a23deae5f5906518402f
Daniel Kempkens <daniel+git@kempkens.io>
2023-11-23 16:45:00 +0100

    tabnine: init

---
+++ packages/tabnine.nix
@@ -0,0 +1,19 @@
+{ pkgs, lib, ... }:
+
+pkgs.tabnine.overrideAttrs (
+  oa: rec {
+    version = "4.49.0";
+
+    src = pkgs.fetchurl {
+      url = "https://update.tabnine.com/bundles/${version}/aarch64-apple-darwin/TabNine.zip";
+      sha256 = "0wrzbv2mam83visp322vwg98f63wcxxwlwm5rzwyd0psgqmgjkb3";
+    };
+
+    meta = with lib; {
+      homepage = "https://tabnine.com";
+      description = "Smart Compose for code that uses deep learning to help you write code faster";
+      license = licenses.unfree;
+      platforms = [ "aarch64-darwin" ];
+    };
+  }
+)
