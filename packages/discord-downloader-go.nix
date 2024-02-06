c15eaf8eaa049f955b6be34e5f413e1bb6726845
Daniel Kempkens <daniel+git@kempkens.io>
2024-02-06 17:32:24 +0100

    ddg: use dev

--- packages/discord-downloader-go.nix
+++ packages/discord-downloader-go.nix
@@ -2,16 +2,16 @@
 
 pkgs.buildGo121Module rec {
   pname = "discord-downloader-go";
-  version = "2.4.0";
+  version = "2.5.0-dev";
 
   src = pkgs.fetchFromGitHub {
     owner = "get-got";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-83B+sFnDE3tDdDT+4C1Sqf3zNN4J+gz0g55yDiV5ngg=";
+    rev = "192d54c4ebdaeaf7e6e094bd59121ae13d50aede";
+    hash = "sha256-HF2qRfD08XSWmciS2wenAP45aNADx6LA++NtVRs1LKY=";
   };
 
-  vendorHash = "sha256-2czUty8sh2CJxukIE77RrlCHNfhB3qUYmFAUy/ehz6s=";
+  vendorHash = "sha256-7H42mz2GxSg3/dw92lX+HHxEHdJp/0AoIbUEGN3JX9c=";
 
   doCheck = false;
 
