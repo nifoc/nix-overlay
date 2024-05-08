12e5a42ed5b4dac0b967b9ffe65d4c620fee2815
Daniel Kempkens <daniel+git@kempkens.io>
2024-05-08 22:06:42 +0200

    chore: switch from import to callPackage

--- flake.nix
+++ flake.nix
@@ -27,28 +27,29 @@
       perSystem = { inputs', system, config, pkgs, lib, ... }: {
         packages =
           let
+            inherit (pkgs) callPackage;
+
             darwinPackages =
-              if lib.hasSuffix "darwin" system then rec {
-                cliclick = import ./packages/cliclick.nix { inherit pkgs lib; };
-                phantomjs = import ./packages/phantomjs.nix { inherit pkgs lib; };
-                tabnine = import ./packages/tabnine { inherit pkgs lib; };
+              if lib.hasSuffix "darwin" system then {
+                cliclick = callPackage ./packages/cliclick.nix { };
+                phantomjs = callPackage ./packages/phantomjs.nix { };
+                tabnine = callPackage ./packages/tabnine { };
               } else { };
           in
           {
-            anonymous-overflow = import ./packages/anonymous-overflow.nix { inherit pkgs lib; };
-            bulk-downloader-for-reddit = import ./packages/bulk-downloader-for-reddit.nix { inherit pkgs lib; };
-            discord-downloader-go = import ./packages/discord-downloader-go.nix { inherit pkgs lib; };
-            fennel-ls = import ./packages/fennel-ls.nix { inherit pkgs lib; };
-            headscale-ui = import ./packages/headscale-ui.nix { inherit pkgs lib; };
-            lexical = import ./packages/lexical.nix { inherit pkgs lib; };
-            neovim-nightly = import ./packages/neovim-nightly.nix { inherit (inputs'.neovim-flake.packages) neovim; inherit pkgs lib; };
-            q = import ./packages/q.nix { inherit pkgs lib; };
-            redlib = import ./packages/redlib.nix { inherit pkgs lib; };
-            rexit = import ./packages/rexit.nix { inherit pkgs lib; };
-            tg-archive = import ./packages/tg-archive.nix { inherit pkgs lib; };
-            vuetorrent = import ./packages/vuetorrent.nix { inherit pkgs lib; };
-
-            weewx-proxy = import ./packages/weewx-proxy.nix { inherit pkgs lib; };
+            anonymous-overflow = callPackage ./packages/anonymous-overflow.nix { };
+            bulk-downloader-for-reddit = callPackage ./packages/bulk-downloader-for-reddit.nix { };
+            discord-downloader-go = callPackage ./packages/discord-downloader-go.nix { };
+            fennel-ls = callPackage ./packages/fennel-ls.nix { };
+            headscale-ui = callPackage ./packages/headscale-ui.nix { };
+            lexical = callPackage ./packages/lexical.nix { };
+            neovim-nightly = callPackage ./packages/neovim-nightly.nix { inherit (inputs'.neovim-flake.packages) neovim; };
+            q = callPackage ./packages/q.nix { };
+            redlib = callPackage ./packages/redlib.nix { };
+            rexit = callPackage ./packages/rexit.nix { };
+            tg-archive = callPackage ./packages/tg-archive.nix { };
+            vuetorrent = callPackage ./packages/vuetorrent.nix { };
+            weewx-proxy = callPackage ./packages/weewx-proxy.nix { };
           } // darwinPackages;
 
         overlayAttrs = config.packages;
--- packages/neovim-nightly.nix
+++ packages/neovim-nightly.nix
@@ -1,13 +1,6 @@
 { neovim, pkgs, lib, ... }:
 
 let
-  latest-libvterm = pkgs.libvterm-neovim.overrideAttrs (_: rec {
-  version = "0.3.3";
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/neovim/libvterm/archive/v${version}.tar.gz";
-      sha256 = "0babe3ab42c354925dadede90d352f054aa9c4ae6842ea803a20c9741e172e56";
-    };
-  });
+  latest-libvterm = pkgs.libvterm-neovim;
 in
 neovim.override { libvterm-neovim = latest-libvterm; }
