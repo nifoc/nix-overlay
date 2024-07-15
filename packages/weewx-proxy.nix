80d9114501e15b0221c13f7c1313e59c5728ee5e
Daniel Kempkens <daniel+git@kempkens.io>
2024-07-15 21:18:12 +0200

    weewx-proxy: update

--- packages/weewx-proxy.nix
+++ packages/weewx-proxy.nix
@@ -12,8 +12,8 @@ beamPackages.mixRelease rec {
     domain = "git.kempkens.io";
     owner = "daniel";
     repo = pname;
-    rev = "7fba9080b4aebb84de2f946934933963bd9fa5a6";
-    sha256 = "sha256-DKQp9X0FtkURw6mFIkojn1zZnqn0zV318F6++SED8uc=";
+    rev = "2350a7859d05b84f270c0fde33bada8f8a09fb1c";
+    sha256 = "sha256-/Wm+nrFslO2vXJc+tH6d2SC9pCVjss4J0x/nXR5T4+0=";
   };
 
   mixNixDeps = import "${src}/deps.nix" { inherit lib beamPackages; };
