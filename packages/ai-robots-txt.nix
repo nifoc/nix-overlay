36fd068cb15ce0a38c26368732bb458977053052
Daniel Kempkens <daniel+git@kempkens.io>
2025-03-16 21:46:30 +0100

    ai-robots-txt: add generic version

--- packages/ai-robots-txt.nix
+++ packages/ai-robots-txt.nix
@@ -13,7 +13,15 @@ pkgs.stdenvNoCC.mkDerivation rec {
 
   installPhase = ''
     runHook preInstall
+
+    cp robots.txt robots_generic.txt
+    echo "" >> robots_generic.txt
+    echo "User-agent: *" >> robots_generic.txt
+    echo "Disallow: /" >> robots_generic.txt
+
     install -D robots.txt $out/share/robots.txt
+    install -D robots_generic.txt $out/share/robots_generic.txt
+
     runHook postInstall
   '';
 }
