d237b4e0f986a3f44f736685f5cbecb43d4b34cb
Daniel Kempkens <daniel+git@kempkens.io>
2026-09-01 20:11:26 +0200

    chore(ci): switch to woodpecker

--- flake.nix
+++ flake.nix
@@ -37,7 +37,6 @@
               callBeamPackage = pkgs.beamMinimalPackages.callPackage;
 
               callPythonPackage = pkgs.python3.pkgs.callPackage;
-              callPython310Package = pkgs.python310.pkgs.callPackage;
               callPython312Package = pkgs.python312.pkgs.callPackage;
             in
             {
@@ -56,7 +55,7 @@
               rift = callPackage ./packages/rift.nix { };
               slurp-gts = callPackage ./packages/slurp-gts.nix { };
               raspberry-pi4-uefi-fw = callPackage ./packages/raspberry-pi4-uefi-fw.nix { };
-              tg-archive = callPython310Package ./packages/tg-archive.nix { };
+              tg-archive = callPython312Package ./packages/tg-archive.nix { };
               torrent-best-blocklist = callPackage ./packages/torrent-best-blocklist.nix { };
               vuetorrent = callPackage ./packages/vuetorrent.nix { };
               weewx = callPython312Package ./packages/weewx.nix { };
